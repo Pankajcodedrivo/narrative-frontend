81f2e25f8c0f0bed2c161a80d24cab17a4b32c82
Pankajcodedrivo <pankaj.expdeveloper@gmail.com>
2026-02-05 18:20:48 +0530

    updatedCode

---
+++ src/pages/auth/Login/useLogin.ts
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { emailSchema, passwordSchema } from "../../../../utils/yupSchemas";
+import { logInApi } from "../../../../services/apis/auth.api";
+import { useDispatch } from "react-redux";
+import { setUser } from "../../../../store/auth.store";
+import { useNavigate } from "react-router-dom";
+
+export const useLogin = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  const loginFormik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: yup.object({
+      email: emailSchema,
+      password: passwordSchema,
+    }),
+onSubmit: async (values) => {
+  setLoading(true);
+  try {
+    const bodyData = {
+      email: values.email,
+      password: values.password,
+    };
+
+    const response = await logInApi(bodyData);
+
+    const user = response?.data?.user;
+
+    // 🔴 ADD THIS (REQUIRED for RequireRole)
+    localStorage.setItem("user", JSON.stringify(user));
+
+    dispatch(setUser(user));
+
+    localStorage.setItem("access_token", response?.data?.tokens.access);
+    localStorage.setItem("refresh_token", response?.data?.tokens.refresh);
+
+    if (user?.role === "admin") {
+      navigate("/admin/report-library", { replace: true });
+    } else {
+      navigate("/client/report-library", { replace: true });
+    }
+
+  } catch (error) {
+    console.error(error);
+  } finally {
+    setLoading(false);
+  }
+},
+  });
+  return {
+    loginFormik,
+    loading,
+  };
+};
